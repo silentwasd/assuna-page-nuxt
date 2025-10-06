4454e541a717adc19ff0960c4029c7c48f0ddb09
troxi <sergeycortez@gmail.com>
2025-10-06 14:40:35 +0300

    useauth update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+    devtools: { enabled: true },
 
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
+    modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
 
-  ssr: false,
+    ssr: false,
 
-  i18n: {
-    locales: ["en-US", "ru-RU", "ja-JP"],
-    defaultLocale: "ru-RU",
-    detectBrowserLanguage: false,
-    vueI18n: "./i18n.config.ts",
-    strategy: 'no_prefix'
-  },
+    i18n: {
+        locales: ["en-US", "ru-RU", "ja-JP"],
+        defaultLocale: "ru-RU",
+        detectBrowserLanguage: false,
+        vueI18n: "./i18n.config.ts",
+        strategy: 'no_prefix'
+    },
 
-  app: {
-    head: {
-      script: [{
-        src: '/js/metrics.js'
-      }]
+    app: {
+        head: {
+            script: [{
+                src: '/js/metrics.js'
+            }]
+        }
     }
-  }
 })
